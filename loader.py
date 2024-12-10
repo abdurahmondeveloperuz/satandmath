97a45ea3fcc2481831b31c3f6147e3b12791a022
abdurahmondeveloperuz <154701401+abdurahmondeveloperuz@users.noreply.github.com>
2024-12-10 21:02:41 +0500

    Update loader.py

--- loader.py
+++ loader.py
@@ -4,6 +4,8 @@ from utils.db_api.sqlite import Database, TestManager, UserRankings
 
 
 from data import config
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+
 
 bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.HTML)
 storage = MemoryStorage()
@@ -11,3 +13,4 @@ dp = Dispatcher(bot, storage=storage)
 db = Database(path_to_db="data/main.db")
 testmgr = TestManager(path_to_db="data/testmanager.db")
 rankings = UserRankings(path_to_db="data/testmanager.db")
+sched = AsyncIOScheduler()
